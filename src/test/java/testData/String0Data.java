5783edfa58a6b0f63cdd3e73ec67d53977be9136
Terentev Denis <terentyevdenis7@gmail.com>
2020-10-28 23:16:35 +0300

    first task, add test system and 0.String, some refactoring

--- src/test/java/testData/String0Data.java
+++ src/test/java/testData/String0Data.java
@@ -7,10 +7,9 @@ import java.util.stream.Stream;
 
 import static utils.FileReader.getTestValues;
 
-public class StringData {
-    @DataSupplier(name = "String")
+public class String0Data {
+    @DataSupplier(name = "String0")
     public Stream<TestData<String,Integer>> supplier(){
-        //return Stream.of(new TestData<>("123",3));
         return getTestValues("src/main/resources/0.String").map(testValue -> new TestData<String, Integer>(testValue.getInputString(),Integer.valueOf(testValue.getExpectedResult())));
     }
 }
